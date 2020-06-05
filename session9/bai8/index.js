5c02f0030c07556dd0d6f4bcea5f6b47d2fa423d
lovepsm94 <hung.hust258@gmail.com>
2020-06-06 03:44:34 +0700

    session 9 homeworks

--- session9/bai8/index.js
+++ session9/bai8/index.js
@@ -26,7 +26,7 @@ function makeId(length) {
 addBtn.addEventListener('click', () => {
     async function postItem() {
         let id = makeId(10);
-        const newItem = {"data":[{ "id": id, "name": `${itemInput.value}`}]}
+        const newItem = {"data":[{ "id": id, "name": itemInput.value }]}
         const response = await fetch('https://sheetdb.io/api/v1/pjzxoijmk9an5', {
             body: JSON.stringify(newItem),
             method: 'POST',
@@ -35,8 +35,10 @@ addBtn.addEventListener('click', () => {
             }
         });
         showListItems();
+        
     }
     postItem();
+    itemInput.value = '';
 });
 async function deleteItem(id) {
     const response = await fetch(`https://sheetdb.io/api/v1/pjzxoijmk9an5/id/${id}`, {
@@ -46,4 +48,24 @@ async function deleteItem(id) {
             }
     });
     showListItems();
+    itemInput.value = '';
 }
+
+async function searchItem() {
+    itemInput.addEventListener('keydown', async function xx() {
+        const items = await getData();
+        let x = [];
+        for(let i = 0; i < items.length; i++){
+            let itemsLowerCase = items[i].name.toLowerCase();
+            let characters = itemInput.value.toLowerCase();
+            if (itemsLowerCase.includes(characters)) {
+                x.push({id : items[i].id, name : items[i].name});
+            }
+        }
+        ol.innerHTML = '';
+        for (let i = 0; i < x.length; i++) {
+            ol.insertAdjacentHTML('beforeend', `<li>${x[i].name}<button onclick="deleteItem('${x[i].id}')">x</button></li>`);
+        }
+    })
+}
+searchItem();
